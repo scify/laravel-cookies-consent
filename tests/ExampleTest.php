48ad1b1657b0acd4917b307691fb12dbe4de32d9
Paul Isaris <paulisaris@gmail.com>
2022-11-18 12:30:19 +0200

    Added test for translations

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,3 +3,8 @@
 it('can test', function () {
     $this->assertTrue(true);
 });
+
+it('provides translations', function () {
+    $this->assertTranslationExists('cookies_consent::messages.title');
+    $this->assertTranslationExists('cookies_consent::messages.body');
+});
