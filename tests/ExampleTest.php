641f459c40cc945104b17bed367628e0ca1038ba
Paul Isaris <paulisaris@gmail.com>
2022-11-18 12:30:28 +0200

    Removed initial test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,9 +1,5 @@
 <?php
 
-it('can test', function () {
-    $this->assertTrue(true);
-});
-
 it('provides translations', function () {
     $this->assertTranslationExists('cookies_consent::messages.title');
     $this->assertTranslationExists('cookies_consent::messages.body');
