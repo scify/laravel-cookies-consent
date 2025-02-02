7fca3f51562b5a2c41b49048888a7daa5726ea64
Paul Isaris <paulisaris@gmail.com>
2025-02-02 11:17:54 +0200

    refactoring

--- resources/js/cookies-consent.js
+++ resources/js/cookies-consent.js
@@ -182,9 +182,10 @@ function getCookie(name) {
     const nameEQ = name + "=";
     const ca = document.cookie.split(';');
     for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        let c = ca[i].trim();
+        if (c.startsWith(nameEQ)) {
+            return c.substring(nameEQ.length, c.length);
+        }
     }
     return null;
 }
